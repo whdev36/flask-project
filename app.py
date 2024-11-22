bb205b460c5183131e38d2e98a454e1069dd89a9
whdev36 <wisdomhunter36@gmail.com>
2024-11-22 10:47:08 -0800

    name form
    Name Form was created with WTF

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import (Flask, render_template, request, current_app,
 				make_response, redirect, abort, url_for)
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from uuid import uuid4
 
 # Configure app
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+app.config['SECRET_KEY'] = 'dfdfdf'
 
-@app.route('/')
+class NameForm(FlaskForm):
+	name = StringField('What\'s your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def get_user(name):
