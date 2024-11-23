211e5ef9e5f7045e0a02003bfca8bbcedab4cd3d
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 14:26:45 -0800

    templating

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'CREATE_YOUR_SECRET_KEY'
 
     from .views import views
 
