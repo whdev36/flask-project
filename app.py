99e012e7788ecfea32dc310a8f763f25282c12d7
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 08:00:53 -0800

    database

--- app.py
+++ app.py
@@ -4,15 +4,42 @@ from flask import (Flask, render_template, request, current_app,
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Configure app
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY' # Create a your SECRET KEY!
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db =SQLAlchemy(app) # DB configure
 
 class NameForm(FlaskForm):
 	name = StringField('What\'s your name?', validators=[DataRequired()])
 	submit = SubmitField('Submit')
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
