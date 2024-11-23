ecff80b058e59b36bd727e8739e4c4c0bac5b3a5
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 13:52:56 -0800

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-from flask_mail import Message
-from app import mail
-
-msg = Message('test mail', sender='diyorbekqodirboyev@gmail.com', recipients=['d.zcode4@gmail.com'])
-msg.body = 'text body'
-msg.html = 'text <b>html</b>'
-mail.send(msg)
