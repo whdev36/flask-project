98f059aedcf77b129bf1ea0fdc15afcb1a64c20d
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 17:50:51 -0800

    register and main alerts

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        pass
     return render_template('login.html')
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email', '')
+        first_name = request.form.get('first-name', '')
+        last_name = request.form.get('last-name', '')
+        password = request.form.get('password', '')
+        confirm_password = request.form.get('confirm-password', '')
+
+        if password != confirm_password:
+            flash('Password don\'t match.', 'warning')
+        else:
+            flash('Account created!', 'success')
+
     return render_template('register.html')
 
 @auth.route('/logout')
