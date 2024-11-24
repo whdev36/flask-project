045b25b188f09d462816d69ba144654fcd6d2053
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 16:37:16 -0800

    authentication routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'CREATE_YOUR_SECRET_KEY'
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views)
+    app.register_blueprint(auth)
 
     return app
