7b474a22ce10c6d635a2fdba5c68d952cd269a24
whdev36 <wisdomhunter36@gmail.com>
2024-11-23 19:53:10 -0800

    note form

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    if request.method == 'POST':
+        note = request.form.get('note', '')
+        new_note = Note(data=note, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added!', 'success')
+        return redirect(url_for('views.index'))
     return render_template('index.html')
